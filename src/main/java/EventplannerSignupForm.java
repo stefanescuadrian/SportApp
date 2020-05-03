a677140295c37018f9ca023db5142de6dee15dd1
stefanescuadrian <stefanescuadrian04@gmail.com>
2020-05-03 11:36:18 +0300

    ESPORT-23 (Save Users to Database)

--- src/main/java/EventplannerSignupForm.java
+++ src/main/java/EventplannerSignupForm.java
@@ -4,13 +4,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
+
 public class EventplannerSignupForm {
+    private Alert alert = new Alert(Alert.AlertType.INFORMATION);
+    public TextField firstNameEP;
+    public TextField lastNameEP;
+    public TextField emailAddressEP;
+    public PasswordField passwordEP;
+    public TextField phoneNumberEP;
+    public DatePicker dateOfBirthEP;
+
 
     @FXML
     private Label signupLabel;
@@ -21,10 +31,6 @@ public class EventplannerSignupForm {
     @FXML
     private Button signupButtonForm;
 
-    @FXML
-    void saveSportsmanData(ActionEvent event) {
-
-    }
 
     @FXML
     void changeScreenButtonPushed(ActionEvent event) throws IOException {
@@ -34,5 +40,137 @@ public class EventplannerSignupForm {
         window.setScene(signupScene);
         window.show();
     }
+    public void saveEventPlannerData(ActionEvent e) throws IOException {
+
+        if (firstNameEP.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your first name!");
+            alert.showAndWait();
+            return;
+        }
+        if (lastNameEP.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your last name!");
+
+            alert.showAndWait();
+            return;
+        }
+        if (emailAddressEP.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your email!");
+
+            alert.showAndWait();
+            return;
+        }
+        if (passwordEP.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a password!");
+            alert.showAndWait();
+            return;
+        }
+        if (phoneNumberEP.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your phone number!");
+            alert.showAndWait();
+            return;
+        }
+
+        if (dateOfBirthEP.getValue()==null){
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please select your birthdate!");
+            alert.showAndWait();
+            return;
+        }
+
+        alert.setTitle("Registration Successfull");
+        alert.setHeaderText(null);
+        alert.setContentText("Welcome " + firstNameEP.getText() + " " + lastNameEP.getText() + "!");
+        alert.showAndWait();
+
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("EventPlanner_signupuriInfo.txt", true))) {
+            bw.write("Role: Sportsman ");
+            bw.newLine();
+            bw.write("First Name: " + firstNameEP.getText() + " ");
+            bw.newLine();
+            bw.write("Last Name: " + lastNameEP.getText() + " ");
+            bw.newLine();
+            bw.write("Email: " + emailAddressEP.getText() + " ");
+            bw.newLine();
+            bw.write("Password: " + passwordEP.getText());
+            bw.newLine();
+            bw.write("Phone Number: " + phoneNumberEP.getText());
+            bw.newLine();
+            bw.write("BirthDate: " + dateOfBirthEP.getValue().toString());
+            bw.newLine();
+            bw.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_firstName.txt", true))) {
+            bw2.write(firstNameEP.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_lastName.txt", true))) {
+            bw2.write(lastNameEP.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_email.txt", true))) {
+            bw2.write(emailAddressEP.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_passwords.txt", true))) {
+            bw2.write(passwordEP.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_phoneNumbers.txt", true))) {
+            bw2.write(phoneNumberEP.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("EventPlanner_birthDates.txt", true))) {
+            bw2.write(dateOfBirthEP.getValue().toString());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+
+        firstNameEP.clear();
+        lastNameEP.clear();
+        emailAddressEP.clear();
+        passwordEP.clear();
+        phoneNumberEP.clear();
+        dateOfBirthEP.setValue(null);
+
+        Parent signupView= FXMLLoader.load(getClass().getResource("/login.fxml"));
+        Scene signupScene=new Scene(signupView);
+        Stage window=(Stage)((Node)e.getSource()).getScene().getWindow();
+        window.setScene(signupScene);
+        window.show();
+
+    }
+
 
 }
--- src/main/java/SportsmanSignupForm.java
+++ src/main/java/SportsmanSignupForm.java
@@ -4,13 +4,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
+
 public class SportsmanSignupForm {
+
+
+    public TextField firstName;
+    public TextField lastName;
+    public TextField emailAddress;
+    public PasswordField password;
 
     @FXML
     private Label signupLabel;
@@ -19,10 +27,10 @@ public class SportsmanSignupForm {
     private Button signupButtonForm;
     @FXML
     private Button goBackButton;
-    @FXML
-    void saveSportsmanData(ActionEvent event) {
+    private Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
-    }
+    @FXML
+
     public void changeScreenButtonPushed(ActionEvent event) throws IOException {
         Parent signupView= FXMLLoader.load(getClass().getResource("/signup.fxml"));
         Scene signupScene=new Scene(signupView);
@@ -30,4 +38,100 @@ public class SportsmanSignupForm {
         window.setScene(signupScene);
         window.show();
     }
+
+    public void saveSportsmanData(ActionEvent e) throws IOException {
+
+        if (firstName.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your first name!");
+            alert.showAndWait();
+            return;
+        }
+        if (lastName.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your last name!");
+
+            alert.showAndWait();
+            return;
+        }
+        if (emailAddress.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your email!");
+
+            alert.showAndWait();
+            return;
+        }
+        if (password.getText().isEmpty()) {
+            alert.setTitle("ERROR");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter a password!");
+            alert.showAndWait();
+            return;
+        }
+
+        alert.setTitle("Registration Successfull");
+        alert.setHeaderText(null);
+        alert.setContentText("Welcome " + firstName.getText() + " " + lastName.getText() + "!");
+        alert.showAndWait();
+
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Sportsman_signupuriInfo.txt", true))) {
+            bw.write("Role: Sportsman ");
+            bw.newLine();
+            bw.write("First Name: " + firstName.getText() + " ");
+            bw.newLine();
+            bw.write("Last Name: " + lastName.getText() + " ");
+            bw.newLine();
+            bw.write("Email: " + emailAddress.getText() + " ");
+            bw.newLine();
+            bw.write("Password: " + password.getText());
+            bw.newLine();
+            bw.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("Sportsman_firstName.txt", true))) {
+            bw2.write(firstName.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("Sportsman_lastName.txt", true))) {
+            bw2.write(lastName.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("Sportsman_email.txt", true))) {
+            bw2.write(emailAddress.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try (BufferedWriter bw2 = new BufferedWriter(new FileWriter("Sportsman_passwords.txt", true))) {
+            bw2.write(password.getText());
+            bw2.newLine();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        firstName.clear();
+        lastName.clear();
+        emailAddress.clear();
+        password.clear();
+
+        Parent signupView= FXMLLoader.load(getClass().getResource("/login.fxml"));
+        Scene signupScene=new Scene(signupView);
+        Stage window=(Stage)((Node)e.getSource()).getScene().getWindow();
+        window.setScene(signupScene);
+        window.show();
+    }
 }
+
